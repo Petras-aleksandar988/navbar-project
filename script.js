72e851abd97473154095838c7c515db16edd69ec
Petras-aleksandar988 <nosallc10@gmail.com>
2023-02-16 18:01:53 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".toggle-button");
+const navlinks = document.querySelector(".nav-links")
+button.addEventListener("click", () => {
+    navlinks.classList.toggle("active")
+})
